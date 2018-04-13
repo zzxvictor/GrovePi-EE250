b55f2523756c1e4d8ad095c59fa829eb263abe5e
Zixuan Victor Zhang <35340469+zzxvictor@users.noreply.github.com>
2018-04-13 15:56:29 -0700

    Update rpiCode.py

--- ee250/lab09/rpiCode.py
+++ ee250/lab09/rpiCode.py
@@ -59,5 +59,5 @@ if __name__ == '__main__':
         #read humidity sensor
         #publish it 
         #print (t.getTemperature())
-        client.publish("anrg-pi1/temperature", "hello")
+        client.publish("anrg-pi1/humidity", "hello")
         time.sleep(1)
